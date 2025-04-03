0218a3c252810f212636d5ba92a2ea82be341f7f
Masum <hydrapikachu29@gmail.com>
2025-04-03 22:40:13 +0530

    Combined packages and merged them to one MainActivity

--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -6,16 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContent {
             Column(
                 modifier = Modifier
@@ -26,10 +30,20 @@ class MainActivity : ComponentActivity() {
             ) {
                 Text("I'm Masum and ")
                 Text("I love Saheli.")
+                Row(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(300.dp)
+                        .background(Color.Cyan)
+                        .padding(20.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("I")
+                    Text("Love")
+                    Text("Saheli")
+                }
             }
         }
     }
 }
-
-
-
