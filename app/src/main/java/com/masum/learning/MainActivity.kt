17ac900d0562b9b0f50b29e6fc70ce6812e2a2bb
Masum <hydrapikachu29@gmail.com>
2025-08-02 18:27:53 +0530

    Basic font customisations

--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -3,19 +3,33 @@ package com.masum.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface {
-                }
+            Box( modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF121212))
+            ) {
+                Text(
+                    text = "Why am I doing this shit",
+                    color = Color.Cyan,
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
     }
