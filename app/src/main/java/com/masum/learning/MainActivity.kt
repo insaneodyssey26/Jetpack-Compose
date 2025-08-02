3def020efb358293e2eb2660c68e83e435f9bc43
Masum <hydrapikachu29@gmail.com>
2025-08-02 18:12:23 +0530

    Removed default composables

--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -15,22 +15,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Surface {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
-    }
-}
