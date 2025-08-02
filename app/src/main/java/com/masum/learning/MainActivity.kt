afe2a90b65f7737ec0fcecc6a3343af614d234b3
Masum <hydrapikachu29@gmail.com>
2025-08-02 19:01:17 +0530

    Added fontfamily with naming fixes

--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -19,6 +21,11 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.libertinussans_regular, FontWeight.Normal),
+            Font(R.font.libertinussans_bold, FontWeight.Bold),
+            Font(R.font.libertinussans_italic, FontWeight.Normal)
+        )
         setContent {
             Box( modifier = Modifier
                 .fillMaxSize()
@@ -28,10 +35,9 @@ class MainActivity : ComponentActivity() {
                     text = "Why am I doing this shit",
                     color = Color.Cyan,
                     fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold
+                    fontFamily = fontFamily
                 )
             }
         }
     }
 }
-
