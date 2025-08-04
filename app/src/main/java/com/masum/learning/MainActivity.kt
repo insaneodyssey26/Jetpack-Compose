6a5148a114f0a978cb782cdfa4c9f08f19bbe5dc
Masum <hydrapikachu29@gmail.com>
2025-08-05 01:27:05 +0530

    Wrapped first box in a column

--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,9 +34,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColorBox(
-                Modifier.fillMaxSize()
-            )
+            Column (Modifier.fillMaxSize()) {
+                ColorBox(
+                    Modifier.fillMaxSize()
+                )
+            }
         }
     }
 }
