ec33bb6c7084c2c2de6d2c39393a68f9cb71e052
Masum <hydrapikachu29@gmail.com>
2025-08-06 01:34:50 +0530

    Cleared mainactivity after states

--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -3,13 +3,11 @@ package com.masum.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.masum.learning.Learnings.StatesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StatesScreen()
         }
     }
 }
