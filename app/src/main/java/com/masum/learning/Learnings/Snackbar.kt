2163e13781e2276e8d8ece749eee1c8807300c35
Masum <hydrapikachu29@gmail.com>
2025-08-07 01:47:24 +0530

    Other parameters and scope value

--- app/src/main/java/com/masum/learning/Learnings/Snackbar.kt
+++ app/src/main/java/com/masum/learning/Learnings/Snackbar.kt
@@ -2,9 +2,12 @@ package com.masum.learning.Learnings
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -16,13 +19,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun Snackbar(modifier: Modifier = Modifier) {
     val snackbarHostState = remember { SnackbarHostState() }
-    var textFieldState by remember {
-        mutableStateOf("")
-    }
+    var textFieldState by remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
     Scaffold(
         modifier = modifier.fillMaxSize(),
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
@@ -46,6 +49,14 @@ fun Snackbar(modifier: Modifier = Modifier) {
                 singleLine = true,
                 modifier = Modifier.fillMaxWidth()
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                scope.launch {
+                    snackbarHostState.showSnackbar("Hello $textFieldState")
+                }
+            }) {
+                Text("Greet Me")
+            }
         }
     }
 }
