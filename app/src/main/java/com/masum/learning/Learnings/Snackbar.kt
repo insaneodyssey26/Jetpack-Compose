772fe0378c9ebe95816ac2fb2b4b926a708cd907
Masum <hydrapikachu29@gmail.com>
2025-08-07 01:29:05 +0530

    Basic properties for snackbar

---
+++ app/src/main/java/com/masum/learning/Learnings/Snackbar.kt
@@ -0,0 +1,34 @@
+package com.masum.learning.Learnings
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Snackbar(modifier: Modifier = Modifier) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { paddingValues ->
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 36.dp)
+        ) {
+
+        }
+    }
+}
