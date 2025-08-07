d9ec526d12b5e4e1a380008b7a05edf52ad27a50
Masum <hydrapikachu29@gmail.com>
2025-08-07 19:31:11 +0530

    Non scrollable list

---
+++ app/src/main/java/com/masum/learning/Learnings/Lists.kt
@@ -0,0 +1,30 @@
+package com.masum.learning.Learnings
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Lists (modifier: Modifier = Modifier) {
+    Column {
+        for (i in 1..50) {
+            Text(
+                text  = "Item $i",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+    }
+}
--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -3,13 +3,14 @@ package com.masum.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.masum.learning.Learnings.Lists
 import com.masum.learning.Learnings.Snackbar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Snackbar()
+            Lists()
         }
     }
 }
