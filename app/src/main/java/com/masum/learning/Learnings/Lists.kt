fd238eeffb7c40392f10e8a5f26d0968f2942e46
Masum <hydrapikachu29@gmail.com>
2025-08-07 19:42:06 +0530

    Lazy column implementation

--- app/src/main/java/com/masum/learning/Learnings/Lists.kt
+++ app/src/main/java/com/masum/learning/Learnings/Lists.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -15,15 +16,36 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+//@Composable
+
+// Normal column with scroll
+
+//fun Lists (modifier: Modifier = Modifier) {
+//    val scrollState: ScrollState = rememberScrollState()
+//    Column (
+//        modifier = Modifier.verticalScroll(scrollState)
+//    ) {
+//        for (i in 1..50) {
+//            Text(
+//                text  = "Item $i",
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Bold,
+//                textAlign = TextAlign.Center,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(vertical = 24.dp)
+//            )
+//        }
+//    }
+//}
+
+// LazyColumn
 @Composable
-fun Lists (modifier: Modifier = Modifier) {
-    val scrollState: ScrollState = rememberScrollState()
-    Column (
-        modifier = Modifier.verticalScroll(scrollState)
-    ) {
-        for (i in 1..50) {
+fun Lists(modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(5000) {
             Text(
-                text  = "Item $i",
+                text = "Item $it",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
