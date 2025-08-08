3793546db7e4f41f05db2f0f14bfeb5345d1eb7f
Masum <hydrapikachu29@gmail.com>
2025-08-08 23:05:36 +0530

    Constrain set for green box

--- app/src/main/java/com/masum/learning/Learnings/ConstraintLayout.kt
+++ app/src/main/java/com/masum/learning/Learnings/ConstraintLayout.kt
@@ -2,7 +2,9 @@ package com.masum.learning.Learnings
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 @Composable
 fun ConstraintLayout (modifier: Modifier = Modifier) {
@@ -10,7 +12,10 @@ fun ConstraintLayout (modifier: Modifier = Modifier) {
         val greenBox = createRefFor("greenBox")
         val redBox = createRefFor("redBox")
         constrain(greenBox) {
-
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
         }
     }
 }
