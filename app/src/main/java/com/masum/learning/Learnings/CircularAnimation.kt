acbc601db388af4656ace03ba39070221ef45f76
Masum <hydrapikachu29@gmail.com>
2025-08-21 01:07:44 +0530

    DrawArc inside a canvas

--- app/src/main/java/com/masum/learning/Learnings/CircularAnimation.kt
+++ app/src/main/java/com/masum/learning/Learnings/CircularAnimation.kt
@@ -2,14 +2,21 @@ package com.masum.learning.Learnings
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -40,5 +47,19 @@ fun CircularProgressBar (
     )
     LaunchedEffect(true) {
         isAnimationPlayed = true
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius = 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius *2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * currPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
     }
 }
