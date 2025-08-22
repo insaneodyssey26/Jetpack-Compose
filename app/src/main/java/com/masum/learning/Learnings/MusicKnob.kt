bf45461770eb6a40c269d74d58d5d6a6eb18ded5
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-08-22 13:41:22 +0530

    Required states

--- app/src/main/java/com/masum/learning/Learnings/MusicKnob.kt
+++ app/src/main/java/com/masum/learning/Learnings/MusicKnob.kt
@@ -1,9 +1,26 @@
 package com.masum.learning.Learnings
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 
 @Composable
-fun MusicKnob (modifier: Modifier = Modifier) {
-    
+fun MusicKnob (
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableStateOf(limitingAngle)
+    }
+    var touchY by remember {
+        mutableStateOf(0f)
+    }
+    var centerX by remember {
+        mutableStateOf(0f)
+    }
+    var centerY by remember {
+        mutableStateOf(0f)
+    }
 }
