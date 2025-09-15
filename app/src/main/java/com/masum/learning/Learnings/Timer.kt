00220306df7efc00b69c840c814f409151322ecc
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-15 22:38:03 +0530

    Required parameters for timer

---
+++ app/src/main/java/com/masum/learning/Learnings/Timer.kt
@@ -0,0 +1,20 @@
+package com.masum.learning.Learnings
+
+import android.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Timer(
+    totalTime: Long,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    activeBarColor: Color,
+    modifier: Modifier = Modifier,
+    initialValue: Float = 0f,
+    strokeWidth: Dp = 5.dp
+) {
+    
+}
