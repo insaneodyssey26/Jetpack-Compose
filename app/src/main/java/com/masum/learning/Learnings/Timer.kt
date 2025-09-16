e50bd934b2fffd2169ffb6b262a830f761ed0d61
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-16 19:46:13 +0530

    Canvas and Arc for inactive portion of the bar

--- app/src/main/java/com/masum/learning/Learnings/Timer.kt
+++ app/src/main/java/com/masum/learning/Learnings/Timer.kt
@@ -1,12 +1,19 @@
 package com.masum.learning.Learnings
 
-import android.graphics.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -32,5 +39,25 @@ fun Timer(
     }
     var isTimerRunning by remember {
         mutableStateOf(false)
+    }
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .onSizeChanged {
+                size = it
+            }
+    ){
+        Canvas(
+            modifier = modifier
+        ) {
+            drawArc(
+                color = inactiveBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f,
+                useCenter = false,
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
     }
 }
