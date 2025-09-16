20e911f7294c6fe786e59de2963d25a5b540a247
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-17 01:31:07 +0530

    Added required sin and cos angles to calculate active bar head position

--- app/src/main/java/com/masum/learning/Learnings/Timer.kt
+++ app/src/main/java/com/masum/learning/Learnings/Timer.kt
@@ -10,13 +10,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun Timer(
@@ -66,6 +71,11 @@ fun Timer(
                 size = Size(size.width.toFloat(), size.height.toFloat()),
                 style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
             )
+            val center = Offset(size.width /2f, size.height /2f)
+            val beta = (250f * value + 145f) * (PI/180f).toFloat()
+            val radius = size.width /2f
+            val sideA = cos(beta) * radius
+            val sideB = sin(beta) * radius
         }
     }
 }
--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -3,28 +3,12 @@ package com.masum.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.masum.learning.Learnings.CircularProgressBar
-import com.masum.learning.Learnings.ConstraintLayout
-import com.masum.learning.Learnings.EffectHandlers
-import com.masum.learning.Learnings.Lists
-import com.masum.learning.Learnings.SimpleAnimation
-import com.masum.learning.Learnings.Snackbar
-import com.masum.learning.Learnings.StatesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                CircularProgressBar(percentage = 0.8f, number = 100)
-            }
+
         }
     }
 }
