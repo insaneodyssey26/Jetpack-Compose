a07a4762d15046aa46b9d46731e2a59a2afead02
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-16 13:51:47 +0530

    Total 4 states are needed, added them

--- app/src/main/java/com/masum/learning/Learnings/Timer.kt
+++ app/src/main/java/com/masum/learning/Learnings/Timer.kt
@@ -2,8 +2,13 @@ package com.masum.learning.Learnings
 
 import android.graphics.Color
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -16,5 +21,16 @@ fun Timer(
     initialValue: Float = 0f,
     strokeWidth: Dp = 5.dp
 ) {
-    
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    var value by remember {
+        mutableStateOf(initialValue)
+    }
+    var currentTime by remember {
+        mutableStateOf(totalTime)
+    }
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
 }
