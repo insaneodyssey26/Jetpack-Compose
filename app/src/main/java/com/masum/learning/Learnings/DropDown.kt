8fa31b26d5c4d0ace035d6fd4e9fc6bbd78f02d6
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-21 20:42:35 +0530

    Text row

--- app/src/main/java/com/masum/learning/Learnings/DropDown.kt
+++ app/src/main/java/com/masum/learning/Learnings/DropDown.kt
@@ -2,12 +2,20 @@ package com.masum.learning.Learnings
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DropDownMenu (
@@ -27,4 +35,21 @@ fun DropDownMenu (
         targetValue = if(isOpened) 0f else -90f,
         animationSpec = tween (durationMillis = 300)
     )
+    Column (
+        modifier = modifier
+            .fillMaxWidth()
+    ){
+        Row (
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 16.sp
+            )
+        }
+    }
 }
