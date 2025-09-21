9484f87889cecca750db94221c5a77d77676a840
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-21 16:04:09 +0530

    Drop down menu properties

---
+++ app/src/main/java/com/masum/learning/Learnings/DropDown.kt
@@ -0,0 +1,14 @@
+package com.masum.learning.Learnings
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DropDownMenu (
+    text: String,
+    modifier: Modifier = Modifier,
+    opened: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    
+}
