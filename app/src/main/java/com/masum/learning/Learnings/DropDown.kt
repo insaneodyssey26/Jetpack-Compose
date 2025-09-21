a08de979dd90d4a32de58c6e89f5d000f72c0a58
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-21 19:34:18 +0530

    Added Alpha animation

--- app/src/main/java/com/masum/learning/Learnings/DropDown.kt
+++ app/src/main/java/com/masum/learning/Learnings/DropDown.kt
@@ -1,6 +1,12 @@
 package com.masum.learning.Learnings
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -10,5 +16,11 @@ fun DropDownMenu (
     opened: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    
+    var isOpened by remember {
+        mutableStateOf(opened)
+    }
+    val alpha = animateFloatAsState(
+        targetValue = if(isOpened) 1f else 0f,
+        animationSpec = tween (durationMillis = 300)
+    )
 }
