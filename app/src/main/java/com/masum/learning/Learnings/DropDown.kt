524c0e6d061a9b21146d7079d770e05783a542d0
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-22 00:28:46 +0530

    Dropdown works

--- app/src/main/java/com/masum/learning/Learnings/DropDown.kt
+++ app/src/main/java/com/masum/learning/Learnings/DropDown.kt
@@ -2,17 +2,21 @@ package com.masum.learning.Learnings
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
@@ -28,6 +33,25 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
+@Composable
+fun MainDropDown(modifier: Modifier = Modifier) {
+    Surface (
+        color = Color(0xFF101010),
+        modifier = Modifier.fillMaxSize()
+    ){
+        DropDownMenu(
+            text = "Masum",
+            modifier = Modifier.padding(50.dp)
+        ) {
+            Text(text = "Revealed",
+                modifier = Modifier.fillMaxWidth()
+                    .height(100.dp)
+                    .background(Color.Green)
+            )
+        }
+    }
+}
 @Composable
 fun DropDownMenu (
     text: String,
@@ -81,6 +105,9 @@ fun DropDownMenu (
                     transformOrigin = TransformOrigin(0.5f, 0f)
                     rotationX = rotateX.value
                 }
-        )
+                .alpha(alpha.value)
+        ) {
+            content()
+        }
     }
 }
--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -11,29 +11,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.masum.learning.Learnings.MainDropDown
 import com.masum.learning.Learnings.Timer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface (
-                color = Color(0xFF101010),
-                modifier = Modifier.fillMaxSize()
-            ){
-                Box (
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Timer(
-                        totalTime = 13L * 1000L,
-                        handleColor = Color(0xFF61E88A),
-                        inactiveBarColor = Color(0xFF3D3D3D),
-                        activeBarColor = Color(0xFF61E88A),
-                        modifier = Modifier.size(200.dp)
-                    )
-                }
-            }
+            MainDropDown()
         }
     }
 }
