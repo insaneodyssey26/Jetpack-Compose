5a5596909d31cdf3eea09c8525dd4f442dac6577
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-21 21:21:59 +0530

    Drop down icon with rotation

--- app/src/main/java/com/masum/learning/Learnings/DropDown.kt
+++ app/src/main/java/com/masum/learning/Learnings/DropDown.kt
@@ -2,10 +2,14 @@ package com.masum.learning.Learnings
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
@@ -49,6 +54,16 @@ fun DropDownMenu (
                 text = text,
                 color = Color.White,
                 fontSize = 16.sp
+            )
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Drop Down Arrow",
+                tint = Color.White,
+                modifier = Modifier
+                    .clickable {
+                        isOpened = !isOpened
+                    }
+                    .scale(1f, if (isOpened) -1f else 1f)
             )
         }
     }
