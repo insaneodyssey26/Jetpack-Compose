2e023eda0ab367edf1019795a3a29ee8204f5cf1
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-20 12:52:15 +0530

    Instruction set for dice roll app

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/DiceRoller.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/DiceRoller.kt
@@ -2,9 +2,7 @@ package com.masum.learning.Android_CodeLabs.Unit2
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -18,23 +16,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.masum.learning.R
-
-@Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage()
-}
 
 @Preview
 @Composable
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
         .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+        .wrapContentSize(Alignment.Center)  // center all the content
 ) {
+    // we are using remember to store the state of the dice roll, as the value of result can change
     var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
+    val imageResource = when (result) { // select the image based on the result value
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -42,18 +35,19 @@ fun DiceWithButtonAndImage(
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(
+    Column( // arrange items in a column
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painterResource(imageResource),
-            contentDescription = "Dice 1"
+            painterResource(imageResource), // display the dice image from previously used value (imageResource)
+            contentDescription = "Dice Image"
         )
-        // Spacer(modifier = Modifier.padding(0.dp))
         Button(
+            // when the button is clicked, we change the value of result to a random number between 1 and 6
             onClick = { result = (1..6).random() }
         ) {
+            // this is the text inside the button, we are using string resources for localization support
             Text(stringResource(R.string.roll))
         }
     }
--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.masum.learning.Android_CodeLabs.Unit2.DiceRollerApp
+import com.masum.learning.Android_CodeLabs.Unit2.DiceWithButtonAndImage
 import com.masum.learning.Philip_Lackner.MainDropDown
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                DiceRollerApp()
+            DiceWithButtonAndImage()
         }
     }
 }
