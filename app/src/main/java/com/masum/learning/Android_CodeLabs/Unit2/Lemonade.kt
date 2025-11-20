c44b8bcd05ede7d2d4b79047cfe00d31e5523fb5
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-20 13:48:42 +0530

    feat: add required resources for lemonade app

---
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/Lemonade.kt
@@ -0,0 +1,11 @@
+package com.masum.learning.Android_CodeLabs.Unit2
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Lemonade (
+    modifier: Modifier = Modifier
+) {
+
+}
--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -3,7 +3,6 @@ package com.masum.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.masum.learning.Android_CodeLabs.Unit2.DiceRollerApp
 import com.masum.learning.Android_CodeLabs.Unit2.DiceWithButtonAndImage
 import com.masum.learning.Philip_Lackner.MainDropDown
 
