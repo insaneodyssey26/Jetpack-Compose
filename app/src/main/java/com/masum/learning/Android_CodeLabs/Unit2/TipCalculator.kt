44f052ca3ce9c95ebf1458340489b4c967c7b1fd
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-22 19:54:06 +0530

    Added remember state for the input value

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -54,9 +56,11 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumber(modifier: Modifier = Modifier) {
+    var amountInput = remember { mutableStateOf("0") }
+    //val amountInput = ""
     TextField(
-        value = "",
-        onValueChange = {},
+        value = amountInput.value,
+        onValueChange = {amountInput.value = it},
         modifier = modifier
     )
 }
