b1d57d152362decc315ed9c4adc71fb1ce51d288
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-22 20:27:15 +0530

    By remember function lambda aka kotlin delegation

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -56,11 +58,10 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumber(modifier: Modifier = Modifier) {
-    var amountInput = remember { mutableStateOf("0") }
-    //val amountInput = ""
+    var amountInput by remember { mutableStateOf("") }
     TextField(
-        value = amountInput.value,
-        onValueChange = {amountInput.value = it},
+        value = amountInput,
+        onValueChange = {amountInput = it},
         modifier = modifier
     )
 }
