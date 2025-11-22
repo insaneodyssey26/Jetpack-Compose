cccd855b68513f46699a04dca482b74e76e864ae
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-23 01:25:53 +0530

    Implemented state hoisting to complete the functionality

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.HistoricalChange
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -27,6 +28,9 @@ import com.masum.learning.R
 
 @Composable
 fun TipTimeLayout() {
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -42,11 +46,13 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         EditNumber(
+            value = amountInput,
+            onValueChange = { amountInput = it },
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth())
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -59,11 +65,15 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 }
 
 @Composable
-fun EditNumber(modifier: Modifier = Modifier) {
-    var amountInput by remember { mutableStateOf("") }
+fun EditNumber(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
     TextField(
-        value = amountInput,
-        onValueChange = {amountInput = it},
+        value = value,
+        onValueChange = onValueChange,
         label = {
             Text(stringResource(R.string.bill_amount))
         },
