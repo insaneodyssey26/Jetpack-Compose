4a55ada08ce93ad5f5a3d038fe751e463d32b47b
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-23 00:46:37 +0530

    Textfield label with some other cool stuff

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/TipCalculator.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import java.text.NumberFormat
 import com.masum.learning.R
@@ -62,6 +64,11 @@ fun EditNumber(modifier: Modifier = Modifier) {
     TextField(
         value = amountInput,
         onValueChange = {amountInput = it},
+        label = {
+            Text(stringResource(R.string.bill_amount))
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
     )
 }
