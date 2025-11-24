5d1aabdeb5bb427ffd0090da61865fd4974ab351
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-24 15:50:15 +0530

    Updated function with additional parameter to display rounded up number

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/CustomTip.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/CustomTip.kt
@@ -39,7 +39,7 @@ fun TipTimeLayout() {
     var roundUp by remember { mutableStateOf(false) }
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 15.0
-    val tip = calculateTip(amount, tipPercent)
+    val tip = calculateTip(amount, tipPercent, roundUp)
 
     Column(
         modifier = Modifier
@@ -111,8 +111,15 @@ fun EditNumberField(
     )
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean
+): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
