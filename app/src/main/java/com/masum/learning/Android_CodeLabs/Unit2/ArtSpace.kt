ea310be22cd172c887516c2118e584973af72b19
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-27 18:24:59 +0530

    Getting it yeahh

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/ArtSpace.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/ArtSpace.kt
@@ -1,12 +1,23 @@
 package com.masum.learning.Android_CodeLabs.Unit2
 
+import android.text.Layout
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.masum.learning.R
 
 @Composable
@@ -19,11 +30,26 @@ fun ArtSpaceApp(
     ) {
         Box(
             modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            Image(
-                painter = painterResource(R.drawable.test_image),
-                contentDescription = "Test Image"
-            )
+            Column(
+             //   horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.test_image),
+                    contentDescription = "Test Image",
+                    modifier = Modifier
+                        .padding(30.dp)
+                )
+                Text(
+                    text = "Just trying some stuff to practice Jetpack Compose",
+                    fontSize = 25.sp,
+                    modifier = Modifier
+                        .padding(start = 20.dp, top = 100.dp)
+                )
+            }
         }
     }
 }
