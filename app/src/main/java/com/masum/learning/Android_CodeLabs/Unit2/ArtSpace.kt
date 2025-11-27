ad7e12db03f32239ce006f732f951ba6799f7ad5
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-11-27 19:02:28 +0530

    Material3 card composable to implement a shadow background on the photo

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/ArtSpace.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit2/ArtSpace.kt
@@ -1,29 +1,30 @@
 package com.masum.learning.Android_CodeLabs.Unit2
 
-import android.R.attr.text
-import android.text.Layout
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Color
 import com.masum.learning.R
 
 @Composable
@@ -31,7 +32,7 @@ fun ArtSpaceApp(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
     ) {
 //        Box(
@@ -44,16 +45,30 @@ fun ArtSpaceApp(
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier.weight(1f)
             ) {
-                Image(
-                    painter = painterResource(R.drawable.test_image),
-                    contentDescription = "Test Image",
+                Card(
                     modifier = Modifier
-                        .padding(30.dp)
-                )
+                        .padding(horizontal = 30.dp, vertical = 16.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color.White),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+                ) {
+                    Box(modifier = Modifier.padding(16.dp)) {
+                        Image(
+                            painter = painterResource(R.drawable.test_image),
+                            contentDescription = "Test Image",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1f)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.padding(70.dp))
                 val name = stringResource(R.string.author_name)
                 val year = stringResource(R.string.year)
                 Text(
-                    text = "Just Trying Out Some Some Stuff To Learn Jetpack Compose",
+                    text = "Just Trying Out Some Stuff To Learn Jetpack Compose",
                     fontSize = 30.sp,
                     modifier = Modifier
                         .padding(start = 20.dp, top = 10.dp)
