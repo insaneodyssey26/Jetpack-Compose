462b980d53c6faee7f4d3cec024a7f462dfebb16
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-08 00:17:57 +0530

    Added heroes data class and list of data

---
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/Superhero/model/HeroesDataclass.kt
@@ -0,0 +1,10 @@
+package com.masum.learning.Android_CodeLabs.Unit3.Superhero.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
