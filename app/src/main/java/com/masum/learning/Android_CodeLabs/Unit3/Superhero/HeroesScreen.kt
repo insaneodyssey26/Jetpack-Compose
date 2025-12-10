0fd7126b75d476abf96b025a74f8a9b94e592918
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-10 15:57:20 +0530

    Added the image in a box

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/Superhero/HeroesScreen.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/Superhero/HeroesScreen.kt
@@ -1,18 +1,24 @@
 package com.masum.learning.Android_CodeLabs.Unit3.Superhero
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
@@ -56,6 +62,16 @@ fun SuperheroIcon (
                 Text(
                     text = stringResource(hero.descriptionRes),
                     style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null
                 )
             }
         }
