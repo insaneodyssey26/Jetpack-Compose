c629a7baadb2a5069b8ad26d08b737a31224f5d0
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-10 14:21:56 +0530

    A row inside a card to hold all informations and image

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/Superhero/HeroesScreen.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/Superhero/HeroesScreen.kt
@@ -1,10 +1,25 @@
 package com.masum.learning.Android_CodeLabs.Unit3.Superhero
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.masum.learning.R
 
 @Composable
 fun HeroesList (
@@ -15,6 +30,26 @@ fun HeroesList (
         modifier = modifier
     ) {
         itemsIndexed(heroes) { index, hero ->
+
+        }
+    }
+}
+
+@Composable
+fun SuperheroIcon (
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
+        ) {
 
         }
     }
