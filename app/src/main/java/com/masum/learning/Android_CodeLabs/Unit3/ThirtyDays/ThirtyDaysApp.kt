61773ae40d37b0f95ce6ac18deb027778981348a
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-11 23:19:51 +0530

    Added days related string resources

---
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/ThirtyDays/ThirtyDaysApp.kt
@@ -0,0 +1,19 @@
+package com.masum.learning.Android_CodeLabs.Unit3.ThirtyDays
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EachCard (
+    modifier: Modifier = Modifier
+) {
+    Card (
+        elevation = CardDefaults.cardElevation(5.dp),
+        modifier = modifier
+    ) {
+
+    }
+}
