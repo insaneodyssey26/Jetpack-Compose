39784c7f03151f6495026fa6878e4027df6a7786
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-11 22:41:11 +0530

    Added required shapes for 30 days app

---
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/ThirtyDays/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.masum.learning.Android_CodeLabs.Unit3.ThirtyDays.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(16.dp)
+)
