78327dd68906e084a92cdeb3c1ba561465f757a2
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-14 02:39:56 +0530

    Prepared data repository from data class

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/ThirtyDays/ThirtyDaysRepository.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/ThirtyDays/ThirtyDaysRepository.kt
@@ -4,14 +4,65 @@ import com.masum.learning.R
 
 object ThirtyDaysRepository {
     val characters = listOf(
-        Chars(
-            character = R.string.character1,
+        Characters(
+            characterCount = R.string.character1,
+            character = R.string.himeno,
             charDescription = R.string.himeno_desc,
             image = R.drawable.himeno
         ),
-        Chars(
+        Characters(
+            characterCount = R.string.power,
             character = R.string.character2,
-            charDescription = R.string.maki,
+            charDescription = R.string.power_desc,
+            image = R.drawable.power
+        ),
+        Characters(
+            characterCount = R.string.character3,
+            character = R.string.maki,
+            charDescription = R.string.maki_desc,
             image = R.drawable.maki
+        ),
+        Characters(
+            characterCount = R.string.character4,
+            character = R.string.makima,
+            charDescription = R.string.makima_desc,
+            image = R.drawable.makima
+        ),
+        Characters(
+            characterCount = R.string.character5,
+            character = R.string.marin,
+            charDescription = R.string.marin_desc,
+            image = R.drawable.marin
+        ),
+        Characters(
+            characterCount = R.string.character6,
+            character = R.string.mikasa,
+            charDescription = R.string.mikasa_desc,
+            image = R.drawable.mikasa
+        ),
+        Characters(
+            characterCount = R.string.character7,
+            character = R.string.mitsuha,
+            charDescription = R.string.mitsuha_desc,
+            image = R.drawable.mitsuha
+        ),
+        Characters(
+            characterCount = R.string.character8,
+            character = R.string.nobara,
+            charDescription = R.string.nobara_desc,
+            image = R.drawable.nobara
+        ),
+        Characters(
+            characterCount = R.string.character9,
+            character = R.string.sakura,
+            charDescription = R.string.sakura_desc,
+            image = R.drawable.sakura
+        ),
+        Characters(
+            characterCount = R.string.character10,
+            character = R.string.sakurajima,
+            charDescription = R.string.sakurajima_desc,
+            image = R.drawable.sakurajima
+        )
     )
 }
