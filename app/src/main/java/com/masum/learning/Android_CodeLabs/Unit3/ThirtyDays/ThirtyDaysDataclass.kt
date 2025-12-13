ec7a8a7689b64c98cd77a06acfa6560d1e88fff6
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-14 00:58:51 +0530

    Fixed data class

--- app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/ThirtyDays/ThirtyDaysDataclass.kt
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/ThirtyDays/ThirtyDaysDataclass.kt
@@ -3,7 +3,8 @@ package com.masum.learning.Android_CodeLabs.Unit3.ThirtyDays
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class ThirtyDaysDataclass(
+data class Characters(
+    @StringRes val characterCount: Int,
     @StringRes val character: Int,
     @StringRes val charDescription: Int,
     @DrawableRes val image: Int
---
+++ app/src/main/java/com/masum/learning/Android_CodeLabs/Unit3/ThirtyDays/ThirtyDaysRepository.kt
@@ -0,0 +1,17 @@
+package com.masum.learning.Android_CodeLabs.Unit3.ThirtyDays
+
+import com.masum.learning.R
+
+object ThirtyDaysRepository {
+    val characters = listOf(
+        Chars(
+            character = R.string.character1,
+            charDescription = R.string.himeno_desc,
+            image = R.drawable.himeno
+        ),
+        Chars(
+            character = R.string.character2,
+            charDescription = R.string.maki,
+            image = R.drawable.maki
+    )
+}
