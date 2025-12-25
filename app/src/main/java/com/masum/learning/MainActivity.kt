5ac580e6a54cde8696ad0887600b117457dd65b4
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-12-26 01:36:12 +0530

    Cleaned up code base

--- app/src/main/java/com/masum/learning/MainActivity.kt
+++ app/src/main/java/com/masum/learning/MainActivity.kt
@@ -3,31 +3,12 @@ package com.masum.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.masum.learning.Android_CodeLabs.Unit3.AffirmationList
-import com.masum.learning.Android_CodeLabs.Unit3.AffirmationDatasource
-import com.masum.learning.Android_CodeLabs.Unit3.CoursesCard
-import com.masum.learning.Android_CodeLabs.Unit3.CoursesGrid
-import com.masum.learning.Android_CodeLabs.Unit3.DataSource
-import com.masum.learning.Android_CodeLabs.Unit3.DataSource.courses
-// import com.masum.learning.Android_CodeLabs.Unit3.Superhero.HeroesApp
-// import com.masum.learning.Android_CodeLabs.Unit3.Superhero.HeroesList
-// import com.masum.learning.Android_CodeLabs.Unit3.Superhero.HeroesRepository.heroes
-import com.masum.learning.Android_CodeLabs.Unit3.ThirtyDays.CardsWithList
-import com.masum.learning.ui.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Switch between different CodeLab apps
-            val currentApp = System.getProperty("codelab.app") ?: "affirmations"
 
-            when (currentApp) {
-                "thirtydays" -> CardsWithList()
-                // "superhero" -> HeroesApp()
-                "courses" -> CoursesGrid(coursesList = courses)
-                else -> AffirmationList(affirmationList = AffirmationDatasource().loadAffirmations())
-            }
         }
     }
 }
