705bece67ba63f897d1163b2d7bc0e46913fdc2e
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-01-10 12:32:03 +0530

    Function to pick random word

--- unscramble/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ unscramble/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,8 +1,25 @@
 package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    private fun pickRandomWord(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWord()
+        } else {
+            usedWords.add(currentWord)
+         //   return shuffleCurrentWord(currentWord)
+        }
+    }
+
 }
