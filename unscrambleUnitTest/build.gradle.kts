0d33371a39c0eaf92fa695801c9b00d07c699de3
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-01-14 21:27:33 +0530

    Added JUnit dependency

--- unscrambleUnitTest/build.gradle.kts
+++ unscrambleUnitTest/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    testImplementation("junit:junit:4.13.2")
+
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
