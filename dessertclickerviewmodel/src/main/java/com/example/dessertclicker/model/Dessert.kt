3b1dff93d759a3b76fe8d1857aa11f2b5eef1fa2
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-01-30 20:50:08 +0530

    Fixed package names and added gradle daemon property

---
+++ dessertclickerviewmodel/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -0,0 +1,6 @@
+package com.masum.dessertclickerviewmodel.model
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
