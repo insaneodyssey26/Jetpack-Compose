94f77ef330418027d953ce6ba31eb8dabfa2d0e0
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-03-06 22:55:20 +0530

    Set up the test navhost

--- dessertclicker/src/main/java/com/masum/dessertclicker/MainActivity.kt
+++ dessertclicker/src/main/java/com/masum/dessertclicker/MainActivity.kt
@@ -64,7 +64,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DessertClickerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
