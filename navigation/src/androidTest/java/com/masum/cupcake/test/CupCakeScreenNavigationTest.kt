2fff1ab92af197faba21cd1d4e8b1efb36217a7e
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-03-09 02:47:43 +0530

    No back navigation on start screen

--- navigation/src/androidTest/java/com/masum/cupcake/test/CupCakeScreenNavigationTest.kt
+++ navigation/src/androidTest/java/com/masum/cupcake/test/CupCakeScreenNavigationTest.kt
@@ -3,10 +3,12 @@ package com.masum.cupcake.test
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.masum.navigation.CupcakeApp
 import com.masum.navigation.CupcakeScreen
+import com.masum.navigation.test.R
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -31,5 +33,11 @@ class CupCakeScreenNavigationTest {
     @Test
     fun verifyStartDestination() {
         navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
+
+    @Test
+    fun verifyNoBackNavigationFromStartDestination() {
+        val backText = composeTestRule.activity.getString(com.masum.navigation.R.string.back_button)
+        composeTestRule.onNodeWithContentDescription(backText).assertDoesNotExist()
     }
 }
