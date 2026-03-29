e3bc101037bfa15465335b7af61da723de2e2d8c
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-03-29 19:09:44 +0530

    Added starter code of lunch tray app and fixed package and gradle issues

---
+++ lunchtray/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -0,0 +1,36 @@
+package com.masum.lunchtray
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.masum.lunchtray.ui.OrderViewModel
+
+// TODO: Screen enum
+
+// TODO: AppBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchTrayApp() {
+    // TODO: Create Controller and initialization
+
+    // Create ViewModel
+    val viewModel: OrderViewModel = viewModel()
+
+    Scaffold(
+        topBar = {
+            // TODO: AppBar
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+
+        // TODO: Navigation host
+        // Use uiState and innerPadding to avoid unused warnings
+        val modifier = Modifier.padding(innerPadding)
+    }
+}
